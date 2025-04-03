6de86b88be6900fd07a14a96cae80e2dfd29dc3c
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-03 09:52:11 +0300

    Timer Finished

--- StateAndDataFlow/ContentView.swift
+++ StateAndDataFlow/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    // @StateObject чтоб получать обновления от ObservableObject
+    @StateObject private var timer = TimeCounter() // иниц экземпляр классом
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Vasya!")
+            Text(timer.counter.formatted()) // приводим к стринг
+                .font(.largeTitle) // шрифт
+                .padding(.top, 100) // отступ сверху
+            
+            Spacer() // поднимаем вверх
+            
+            ButtonView(timer: timer) // Binding передаем через $
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ButtonView: View {
+    @ObservedObject var timer: TimeCounter
+    
+    var body: some View {
+        Button(action: timer.startTimer) {
+            Text(timer.buttonTitle) // значение берем из класса
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(Color.red)
+        .clipShape(.rect(cornerRadius: 20))
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.black, lineWidth: 4)
+        )
+    }
 }
--- StateAndDataFlow/TimeCounter.swift
+++ StateAndDataFlow/TimeCounter.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Combine // обработка асинхронных событий и работай с потоками данных
+
+final class TimeCounter: ObservableObject { // отслеживание изменений
+    let objectWillChange = ObservableObjectPublisher()
+    var counter = 3 // уведомляем что значение поменялось
+    var buttonTitle = "Start"
+    
+    private var timer: Timer?
+    
+    func startTimer() {
+        if counter > 0 { // если значение больше 0 то запускаем
+            timer = Timer.scheduledTimer(
+                timeInterval: 1, // интервал в секундах
+                target: self, // цель себя
+                selector: #selector(updateCounter), // действие
+                userInfo: nil, // опускаем
+                repeats: true // повторение
+            )
+        }
+        
+        buttonDidTapped()
+    }
+    
+    @objc private func updateCounter() { // #selector принимает только @objc
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+            buttonTitle = "Reset" // меняем кнопку по завершению таймера
+        }
+        
+        objectWillChange.send() // отправляет обновленное состояние класса
+    }
+    
+    private func killTimer() {
+        timer?.invalidate() // останавливаем
+        timer = nil // выгружаем из памяти
+    }
+    
+    private func buttonDidTapped() {
+        if buttonTitle == "Reset" {
+            counter = 3
+            buttonTitle = "Start"
+        } else {
+            buttonTitle = "Wait..."
+        }
+        
+        objectWillChange.send()
+    }
+}
