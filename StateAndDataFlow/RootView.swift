29689edefa0a905b483499a2ef8dbdaa53fc7b0b
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-04 12:34:11 +0300

    Add Login, Root and UserManager

---
+++ StateAndDataFlow/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexander Shevtsov on 03.04.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
